7fef71c1d54b1cbc84b6e5b45dab5c471154be7c
Swen Mun <longfinfunnel@gmail.com>
2020-07-16 13:38:41 +0900

    WIP2

--- Libplanet.RocksDBStore.Tests/RocksDBStoreTest.cs
+++ Libplanet.RocksDBStore.Tests/RocksDBStoreTest.cs
@@ -5,9 +5,11 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography;
+using System.Text;
 using Bencodex.Types;
 using Libplanet.Action;
 using Libplanet.Blockchain;
+using Libplanet.Net;
 using Libplanet.Tests.Blockchain;
 using Libplanet.Tests.Common.Action;
 using Libplanet.Tests.Store;
@@ -67,8 +69,10 @@ namespace Libplanet.RocksDBStore.Tests
         [Fact]
         public void TestX()
         {
+            var ss = nameof(Swarm<PolymorphicAction<ActionBase>>.StartAsync);
+            var z = ss.Length;
 #pragma warning disable MEN002 // Line is too long
-            var s = new RocksDBStore(@"C:\Users\Swen Mun\Downloads\4a321a45b07750ca7fa88a0a4a0c817fa26c5f5e54ac2ab91675256e6abed21a-snapshot-3e73fe19496ba143ca28949a7241e5bede2d375506b247169b5bf7f762000000");
+            var s = new RocksDBStore(@"C:\Users\X1E\Downloads\4a321a45b07750ca7fa88a0a4a0c817fa26c5f5e54ac2ab91675256e6abed21a-snapshot-3e73fe19496ba143ca28949a7241e5bede2d375506b247169b5bf7f762000000");
 
             var policy = BlockPolicy.GetPolicy(10000);
             var genesis = s.GetBlock<PolymorphicAction<ActionBase>>(
@@ -78,11 +82,15 @@ namespace Libplanet.RocksDBStore.Tests
             var rankState = chain.GetState(RankingState.Address);
             var shopState = chain.GetState(ShopState.Address);
             var configState = chain.GetState(GameConfigState.Address);
+            var waState = chain.GetState(new Address("0000000000000000000000000000000000000011"));
 
             var genState = s.GetBlockStates(genesis.Hash);
             var rankFromGen = genState[RankingState.Address.ToHex()];
             var lastRef = s.LookupStateReference(chain.Id, RankingState.Address.ToHex(), chain.Tip);
             var refs = s.IterateStateReferences(chain.Id, RankingState.Address.ToHex(), chain.Tip.Index, chain.Genesis.Index, 100).ToList();
+
+            var lastWaRef = s.LookupStateReference(chain.Id, "0000000000000000000000000000000000000011", chain.Tip);
+            var waRefs = s.IterateStateReferences(chain.Id, "0000000000000000000000000000000000000011", chain.Tip.Index, 0, 100).ToList();
 
             var blk = s.GetBlock<PolymorphicAction<ActionBase>>(lastRef.Item1);
             var tx = blk.Transactions.First();
@@ -91,7 +99,7 @@ namespace Libplanet.RocksDBStore.Tests
             {
                 BlockIndex = blk.Index,
                 Miner = blk.Miner.Value,
-                PreviousStates = new PrevState(chain, blk.Hash),
+                PreviousStates = new PrevState(chain, chain.Tip.Hash),
                 Random = new Random(0),
                 Rehearsal = false,
                 Signer = tx.Signer,
@@ -102,6 +110,7 @@ namespace Libplanet.RocksDBStore.Tests
             var avatarRefs = s.IterateStateReferences(chain.Id, avatarAddress.ToHex(), chain.Tip.Index, 0, 1000).ToList();
             var lastAgentRef = s.LookupStateReference(chain.Id, tx.Signer.ToHex(), chain.Tip);
             var agentRefs = s.IterateStateReferences(chain.Id, tx.Signer.ToHex(), chain.Tip.Index, 0, 1000).ToList();
+            var avatarState = chain.GetState(avatarAddress);
 
             var blk2 = chain[105157];
             var blk2State = s.GetBlockStates(blk2.Hash);
@@ -110,28 +119,30 @@ namespace Libplanet.RocksDBStore.Tests
 
             var chainIds = s.ListChainIds().ToList();
 
-            var x = new Address("a69c9e583f0f53ebcb4af4816d3fc38571a46716");
+            var x = new Address("b227989ce51fc0ab07c2c5be05ba8062d1a42e8e");
             var lastXRef = s.LookupStateReference(chain.Id, x.ToHex(), chain.Tip);
             var xRefs = s.IterateStateReferences(chain.Id, x.ToHex(), chain.Tip.Index, 0, 1000);
 
             var cf = s.GetColumnFamily(s.StateRefDb, chain.Id);
             var stateRefDb = s.StateRefDb;
             var raw0 = stateRefDb.Get(s.StateRefKey(RankingState.Address.ToHex(), blk.Index), cf);
-            var raw1 = stateRefDb.Get(s.StateRefKey(avatarAddress.ToHex(), blk2.Index), cf);
+            var raw1 = stateRefDb.Get(s.StateRefKey(avatarAddress.ToHex().ToLower(), blk2.Index), cf);
             var raw2 = stateRefDb.Get(s.StateRefKey(GameConfigState.Address.ToHex(), genesis.Index), cf);
 
-            var ks = new List<byte[]>();
+            var ks = new HashSet<string>();
             var prefix = new[] { (byte)'s' }.Concat(RocksDBStoreBitConverter.GetBytes(x.ToHex())).ToArray();
             using (var srIt = stateRefDb.NewIterator(cf))
             {
                 for (srIt.SeekToFirst(); srIt.Valid(); srIt.Next())
                 {
-                    if (srIt.Key().StartsWith(prefix))
+                    if (srIt.Key().StartsWith(new[] { (byte)'s' }))
                     {
-                        ks.Add(srIt.Value());
+                        ks.Add(Encoding.UTF8.GetString(srIt.Key().Skip(1).Take(40).ToArray()));
                     }
                 }
             }
+
+            var exists = ks.Contains(avatarAddress.ToHex());
 
             Assert.False(false);
 #pragma warning restore MEN002 // Line is too long

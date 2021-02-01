db7ff91561c7b685f1cc4370b1009b3dcfc44822
Swen Mun <swen@planetariumhq.com>
2021-02-01 14:53:41 +0900

    Merge pull request #1180 from limebell/feature/ignore-invalidnonce
    Ignore transactions that have lower nonce than the highest nonce among transaction in storage

--- Libplanet/Blockchain/BlockChain.cs
+++ Libplanet/Blockchain/BlockChain.cs
@@ -635,7 +635,8 @@ namespace Libplanet.Blockchain
                     msg);
             }
 
-            if (!StagePolicy.HasStaged(this, transaction.Id, includeUnstaged: true))
+            if (!StagePolicy.HasStaged(this, transaction.Id, includeUnstaged: true)
+                && transaction.Nonce >= Store.GetTxNonce(Id, transaction.Signer))
             {
                 StagePolicy.Stage(this, transaction);
             }
